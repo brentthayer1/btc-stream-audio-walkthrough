8a34b345dd8a8054f04fb3ef2b43d617a215c2ba
Brent Thayer <bthayer1@gmail.com>
2022-02-12 09:40:32 -0700

    test

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,7 +1,6 @@
 export const home = {
 	name: 'Home',
-	description:
-		'This site will help guide through the necesarry steps to set up and maintain proper audio for streaming',
+	description: 'Steps to set up and maintain proper audio for streaming',
 	img: false,
 	process: [],
 };
